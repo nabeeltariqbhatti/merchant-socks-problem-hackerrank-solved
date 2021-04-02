533bb624da596d90834ce9d8857dd150c21b8760
Nabeel Tariq Bhatti <nabeeltaariq@gmail.com>
2021-04-02 06:50:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+vector<string> split_string(string);
 
-int main() {
-
-	//array with capacity to store 1 to 100
-	int freq[100] = { 0 };
-	//to store number of matching socks
-	int result = 0;
-
-	//constant size 0f array
-	const int n = 9;
-
-	int arr[n] = { 10,9,23,23,8,10,20,20,10 };
-
-	//it will simply increase the index of array frequency
-	//for example at first iteration arr[0]=10; and freq[10]=0;
-	//this will increase value of index number 10 by 1 in same process for each
-	for (int i = 0; i < n; i++) {
-	 ++freq[arr[i]];
-	}
-
-
-	//this will simply check for example this will simply return 1,2,3,4 integers decimal value wil be ignored
-
-	//asume we found 9 socks with same color 4.5 = 9/2 integer will ignore after point value and get the 4 out of it so the single one is automatically truncated
-	for (int i = 0; i < 100; i++) {
-		result += freq[i] / 2;
-	}
-
-	cout << result << endl;
+// Complete the sockMerchant function below.
+int sockMerchant(int n, vector<int> ar) {
+  
+  
 
 
 
 
 
-	return 0;
+    return 0;
+}
+
+int main()
+{
+   
+
+    int n;
+    cin >> n;
+
+        int f[101]={0},i;
+        int c=0;
+
+//filling up the frequency array
+
+        for(i=0;i<n;i++)
+        {
+            int num;
+            cin>>num;
+            f[num]++;
+        }
+            
+
+//counting the total number of pairs
+
+        for(i=1;i<=100;i++)
+            c+=f[i]/2;
+
+     cout<<c;
+    
+return 0;
+ 
 }
